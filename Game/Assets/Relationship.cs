d5701056c46ab42ee65a791c6e456236389fec48
Dinerell <33810746+Dinerell@users.noreply.github.com>
2018-07-02 00:58:25 +0200

    Traversierungsvorbereitung
    Abänderung bezüglich eigener Skalierung einzelner Objekte anstatt per Eltern-Objekt

---
+++ Game/Assets/Relationship.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Relationship : MonoBehaviour {
+
+    public List<GameObject> children;
+    public GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //Verknüpft das GameObject mit seinen Kindern
+    void addChild(GameObject child) {
+        children.Add(child);
+    }
+
+    //verknüpft das GameObject mit seinem Elternteil
+    void setParent(GameObject par) {
+        parent = par;
+    }
+
+    void delParent() {
+        parent = null;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Game/Assets/Test.cs
+++ Game/Assets/Test.cs
@@ -3,23 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
+
 	public GameObject obj;
+    public Relationship relation;
 
 	void Start () {
-		
-	}
-		
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space)) {
-			//Wechsel Name aktuelle Instanz
-			obj.transform.name = "Ast";
 
-			//Erzeuge 3 neue Instanzen als Kindobj mit neuem Namen
-			for(int i = 0; i < 3; i++) {
-				obj = Instantiate (obj, transform.position, transform.rotation); //Im Projekt muss das Prefab zugewiesen werden
-				obj.transform.parent = gameObject.transform;
-				obj.transform.name = "Blatt";
-			}
+        //Verbindung mit dem Skript
+        relation = this.GetComponent<Relationship>();
+
+    }
+
+
+    //Erzeugt Kinder
+    void instatiateObject() {
+
+        //Wechsel Name aktuelle Instanz
+        obj.transform.name = "Ast";
+
+        //Erzeuge 3 neue Instanzen als Kindobj mit neuem Namen
+        for (int i = 0; i < 3; i++)
+        {
+
+            //Im Projekt muss das Prefab zugewiesen werden
+            obj = Instantiate(obj, this.transform.position, this.transform.rotation); 
+
+
+            setRelation(obj, gameObject);
+
+            //Übertragen von Position und
+            obj.transform.position = this.transform.position;
+            obj.transform.rotation = this.transform.rotation;
+
+            transformation(obj);
+            
+            obj.transform.name = "Blatt";
+
+        }
+        
+    }
+
+    //Setzt Kind und Elternteil
+    void setRelation(GameObject child, GameObject parent) {
+
+        //Setzt Kind
+        relation.SendMessage("addChild", child);
+
+        //Setzt Elternteil
+        relation.SendMessage("setParent", parent);
+
+    }
+
+    void transformation(GameObject child) {
+
+        //Transformationen hier
+        //obj.transform.Translate(new Vector3(0.0f, 1.0f, 0.0f));
+        obj.transform.localScale = new Vector3(1.0f, 2.0f, 1.0f);
+
+    }
+
+    void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+
+            instatiateObject();
 
 			//Vernichte Script nach einmaligen Einsatz
 			Destroy(GetComponent<Test>());
