9ae98470275a844a9d9670a2027a6314ce922165
fdai4576 <kief1@gmx.net>
2018-07-01 19:29:59 +0200

    Traversierung

--- Game/Assets/Test.cs
+++ Game/Assets/Test.cs
@@ -4,26 +4,22 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 	public GameObject obj;
+
 	void Start () {
 		
 	}
-
-	void grow() {
-		obj = Instantiate (obj, transform.position, transform.rotation); //Im Projekt muss das Prefab zugewiesen werden
-		obj.transform.parent = gameObject.transform;
-		obj.transform.name = "Blatt";
-	}
 		
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			//Wechsel Name aktuelle Instanz
 			obj.transform.name = "Ast";
+
 			//Erzeuge 3 neue Instanzen als Kindobj mit neuem Namen
-			grow();
-			obj.transform.Translate( 2, 0, 0 );
-			//obj.transform.Rotate( 0, 0, 10 );
-			//obj.transform.Translate( 0, 1, 0 );
+			for(int i = 0; i < 3; i++) {
+				obj = Instantiate (obj, transform.position, transform.rotation); //Im Projekt muss das Prefab zugewiesen werden
+				obj.transform.parent = gameObject.transform;
+				obj.transform.name = "Blatt";
+			}
 
 			//Vernichte Script nach einmaligen Einsatz
 			Destroy(GetComponent<Test>());
