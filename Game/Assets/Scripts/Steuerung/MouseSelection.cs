e368e8075a8e2be3b9e2b9c7d19e8422aced8ed5
fdai4576 <kief1@gmx.net>
2018-08-10 22:38:55 +0200

    Update Bugfix

---
+++ Game/Assets/Scripts/Steuerung/MouseSelection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelection : MonoBehaviour {
+
+	void OnMouseEnter(){
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		GetComponent<Renderer>().material.color = Color.green;
+	}
+
+	void OnMouseDown()
+	{
+		Debug.Log ("Mouse is over: " + this.name );
+	}
+}
