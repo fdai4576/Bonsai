8e4c750130dc5ffb187bf04135e1d8b8a9795ae5
fdai4576 <kief1@gmx.net>
2018-08-31 17:23:30 +0200

    Mouse STRG+Rotation

---
+++ Game/Assets/Scripts/Steuerung/MouseSTRG.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSTRG : MonoBehaviour {
+
+	public float inputSensitivity = 500.0f;
+	float mouseX;
+	float mouseY;
+	float rotX = 0.0f;
+	float rotY = 0.0f;
+
+	void Start () {
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotX = rot.x;
+		rotY = rot.y;
+	}
+
+	void Update () {
+		if (Input.GetMouseButton (1)) {
+			Cursor.visible = false;
+			Cursor.lockState = CursorLockMode.Locked;
+			mouseX = Input.GetAxis ("Mouse X");
+			mouseY = Input.GetAxis ("Mouse Y");
+			rotY += mouseX * inputSensitivity * Time.deltaTime;
+			rotX -= mouseY * inputSensitivity * Time.deltaTime;
+
+			Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+			transform.rotation = localRotation;
+		} else {
+			Cursor.visible = true;
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+}
