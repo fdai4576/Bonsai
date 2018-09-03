e9cf6eba0d9c546e3ffb5a4db83b0b48bc1b1e18
fdai4576 <kief1@gmx.net>
2018-09-03 17:15:26 +0200

    Bugfix Selection Color

--- Game/Assets/Scripts/Steuerung/MouseSelection.cs
+++ Game/Assets/Scripts/Steuerung/MouseSelection.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseSelection : MonoBehaviour {
+	Color original;
 
 	void OnMouseEnter(){
+		original = GetComponent<Renderer>().material.color;
 		GetComponent<Renderer>().material.color = Color.red;
 	}
 
 	void OnMouseExit(){
-		GetComponent<Renderer>().material.color = Color.green;
+		GetComponent<Renderer>().material.color = original;
 	}
 
 	void OnMouseDown()
