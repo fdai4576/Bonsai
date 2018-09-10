875c8f861dd8b6c5da8adbabfa23bad31535cdcd
fdai4576 <kief1@gmx.net>
2018-09-10 21:17:40 +0200

    Bugfix selection+IngameMenu

--- Game/Assets/Scripts/Steuerung/MouseSelection.cs
+++ Game/Assets/Scripts/Steuerung/MouseSelection.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class MouseSelection : MonoBehaviour {
 	Color original;
+	bool selected;
 
 	void OnMouseEnter(){
+		selected = true;
 		original = GetComponent<Renderer>().material.color;
 		GetComponent<Renderer>().material.color = Color.red;
 	}
 
 	void OnMouseExit(){
+		selected = false;
 		GetComponent<Renderer>().material.color = original;
 	}
 
@@ -18,5 +21,11 @@ public class MouseSelection : MonoBehaviour {
 	{
 		Debug.Log ("Mouse is over: " + this.name );
         gameObject.AddComponent<Cut_Part>();
+	}
+
+	void Update () {
+		if (Input.GetMouseButton (1) && selected == true) {
+			GetComponent<Renderer>().material.color = original;
+		}
 	}
 }
