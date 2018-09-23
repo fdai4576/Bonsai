71a575c351fb3203a3de435a27bcb3990e36b7c2
Dinerell <33810746+Dinerell@users.noreply.github.com>
2018-09-23 16:23:11 +0200

    Audio
    + zerbrechender Topf und Audio hinzugefügt

---
+++ Game/Assets/Scripts/Baum/BreakPot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakPot : MonoBehaviour {
+    
+    public int space_hits;
+    private GameObject brokenPot;
+
+	// Use this for initialization
+	void Start () {
+        string potPath = "Assets/Prefabs/brokenPot.prefab";
+        brokenPot = (GameObject)UnityEditor.AssetDatabase.LoadAssetAtPath(potPath, typeof(GameObject));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            space_hits++;
+            if (space_hits >= 9) {
+                Instantiate<GameObject>(brokenPot, gameObject.transform.position, gameObject.transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+
+	}
+}
